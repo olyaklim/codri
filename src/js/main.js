b9e86804eaa624aef0e171ba6a0314a4eb2b23b1
Olga Klimenko <olya_klim@ukr.net>
2018-07-08 18:14:23 +0300

    layman

--- src/js/main.js
+++ src/js/main.js
@@ -1,95 +1,7 @@
 
 $(document).ready(function () {
 
-  // lang-list
-  $('.js-lang-list .lang-list__btn').click(function() {
-    $(this).closest('.js-lang-list').toggleClass('active');
-    $(this).closest('.lang-list__item').addClass('active').siblings().removeClass('active');
-  });
-
-
-    
-//  circle-silder
-  $(window).on('load resize', function(){
-    if( $(document).width() > 1030) {
-      var firstTitleSect = $('[data-number="item-1"]').find('.circ-item__title').text()
-      var firstMainImg =   $('.circ-item .circ-item__pic').attr('src');
-
-      // $('.circle-nav__item-title').text(firstTitleSect);
-      $('.circle-nav__pic').attr('src', firstMainImg);
-
-      $('.circ-item').click(function() {
-
-        circ_item_click($(this));
-        
-      });
-
-      $('.circle-nav__photo--top').click(function() {
-        circ_item_click($('.circ-item--top'));
-        
-        $('.circle-nav__photo').removeClass('circle-nav__photo--active-photo');
-
-        $(this).toggleClass('circle-nav__photo--active-photo');
-      });
-
-      $('.circle-nav__photo--left').click(function() {
-        circ_item_click($('.circ-item--left'));
-        $('.circle-nav__photo').removeClass('circle-nav__photo--active-photo');
-        $(this).toggleClass('circle-nav__photo--active-photo');
-      });
-
-      $('.circle-nav__photo--right').click(function() {
-        circ_item_click($('.circ-item--right'));
-        $('.circle-nav__photo').removeClass('circle-nav__photo--active-photo');
-        $(this).toggleClass('circle-nav__photo--active-photo');
-      });
-
-      $('.circle-nav__photo--bottom').click(function() {
-        circ_item_click($('.circ-item--bottom'));
-        $('.circle-nav__photo').removeClass('circle-nav__photo--active-photo');
-        $(this).toggleClass('circle-nav__photo--active-photo');
-      });
-
-      function circ_item_click(circ_btn) {
-
-        $('.circle-nav__item').css('opacity', '0');
-        $('.circle-nav__item').animate({
-          opacity: "1"
-        },500);
-
-        //    id rotate
-        var dataNumb = circ_btn.attr('data-number');
-
-        circ_btn.closest('.slider-list').removeAttr('id');
-        circ_btn.closest('.slider-list').attr('id', dataNumb);
-
-        //    color title
-        $('.circ-list__item').removeClass('active');
-        circ_btn.closest('.circ-list__item').addClass('active');
-
-        //    add title
-        var titleSect = circ_btn.find('.circ-item__title').text();
-
-        // $('.circle-nav__item-title').text('');
-        // $('.circle-nav__item-title').text(titleSect);
-
-        //    add img 
-        var mainImg = circ_btn.find('.circ-item__pic').attr('src');
-
-        $('.circle-nav__pic').attr('src', false);
-        $('.circle-nav__pic').attr('src', mainImg);
-      }
-
-    }
-    
-    if( $(document).width() < 1030  && $(document).width() > 777) { 
-      $('.circ-list__item').equivalent();
-    }
-  });
-
-
-
-  // mob-menu
+   // mob-menu
   if ($(window).width() < 1024) {
     $('.js-parent .main-menu__link').removeAttr('href');
 
@@ -97,7 +9,6 @@ $(document).ready(function () {
       $(this).closest('.js-parent').find('.main-menu__submenu').slideToggle(300);
     });
   }
-
 
   // modal +
   $('[data-modal]').click(function () {
@@ -118,7 +29,44 @@ $(document).ready(function () {
 
     $('[data-id]').removeClass('open');
   });
-  // modal -
-
+  // modal -   
 
 });
+
+
+// YouTube player +
+var player;
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player('player', {
+    events: {
+      // call this function when player is ready to use
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  
+  var pauseButton = document.getElementById("stop");
+  pauseButton.addEventListener("click", function() {
+    player.pauseVideo();
+  });
+
+  var pauseButton = document.getElementById("win3");
+  pauseButton.addEventListener("click", function() {
+    player.pauseVideo();
+  });
+  
+}
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// YouTube player -
+
+
